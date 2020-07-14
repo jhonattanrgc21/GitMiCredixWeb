744ae8df84b81792c78db19c6848c74d6bfe2a3b
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-14 11:06:28 -0400

    Directiva: credix forms para estilo de los campos de texto.

---
+++ src/app/shared/directives/credix-forms/credix-text-field.directive.spec.ts
@@ -0,0 +1,8 @@
+import {CredixTextFieldDirective} from './credix-text-field.directive';
+
+describe('CredixInputTextDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CredixTextFieldDirective();
+    expect(directive).toBeTruthy();
+  });
+});
