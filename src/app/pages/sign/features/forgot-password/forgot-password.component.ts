f4e441440a05308020f46c3bb139b8d4e892782c
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-22 20:40:39 -0400

    Correcciones al componente forgot-password
    -El componente es llamado desde sign-in.

--- src/app/pages/sign/features/forgot-password/forgot-password.component.ts
+++ src/app/pages/sign/features/forgot-password/forgot-password.component.ts
@@ -1,44 +1,39 @@
-import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
-import {
-  FormGroup,
-  Validators,
-  FormControl,
-  ValidatorFn,
-  ValidationErrors,
-} from "@angular/forms";
-//import { Router } from "@angular/router";
-import { finalize } from "rxjs/operators";
-import * as CryptoJS from "crypto-js";
-
-import { ModalService } from "src/app/core/services/modal.service";
-import { HttpService } from "src/app/core/services/http.service";
+import {AfterViewInit, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {finalize} from 'rxjs/operators';
+import * as CryptoJS from 'crypto-js';
+import {ModalService} from 'src/app/core/services/modal.service';
+import {HttpService} from 'src/app/core/services/http.service';
 
 @Component({
-  selector: "app-forgot-password",
-  templateUrl: "./forgot-password.component.html",
-  styleUrls: ["./forgot-password.component.scss"],
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss'],
 })
-export class ForgotPasswordComponent implements OnInit {
-  @ViewChild("forgotPasswordTemplate") forgotPasswordTemplate: TemplateRef<any>;
-  message: string = "";
-  showErrorMessage: boolean = false;
+export class ForgotPasswordComponent implements OnInit, AfterViewInit {
+  @ViewChild('forgotPasswordTemplate') forgotPasswordTemplate: TemplateRef<any>;
+  message = '';
+  showErrorMessage = false;
   identTypes: string[];
   hide = true;
   hide1 = true;
-  identMask = "0-0000-0000";
+  identMask = '0-0000-0000';
   identMaxLength = 0;
-  submitted: boolean = false;
+  submitted = false;
 
-  passwordValidator: ValidatorFn = (
-    control: FormGroup
-  ): ValidationErrors | null => {
-    const password = control.get("password");
-    const repeatPassword = control.get("confirmPassword");
+  constructor(
+    private modalService: ModalService,
+    private httpService: HttpService) {
+  }
+
+  passwordValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+    const password = control.get('password');
+    const repeatPassword = control.get('confirmPassword');
     if (repeatPassword.errors && !repeatPassword.errors.passwordError) {
       return;
     }
     if (password.value !== repeatPassword.value) {
-      repeatPassword.setErrors({ passwordError: true });
+      repeatPassword.setErrors({passwordError: true});
     } else {
       repeatPassword.setErrors(null);
     }
@@ -46,29 +41,27 @@ export class ForgotPasswordComponent implements OnInit {
 
   forgotPassForm: FormGroup = new FormGroup(
     {
-      identType: new FormControl("", [Validators.required]),
-      identNumber: new FormControl("", [Validators.required]),
-      password: new FormControl("", [Validators.required]),
-      confirmPassword: new FormControl("", [Validators.required]),
-      code: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      identType: new FormControl('', [Validators.required]),
+      identNumber: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      code: new FormControl('', [Validators.required, Validators.minLength(6)]),
     },
-    { validators: this.passwordValidator }
+    {validators: this.passwordValidator}
   );
-
-  constructor(
-    private modalService: ModalService,
-    private httpService: HttpService,
-    //private router: Router
-  ) {}
 
   ngOnInit(): void {
     this.getIdentTypes();
   }
 
+  ngAfterViewInit(): void {
+    this.openModal();
+  }
+
   openModal() {
     this.modalService.open(
-      { template: this.forgotPasswordTemplate, title: "¿Olvidó su clave?" },
-      { width: 376, height: 663, disableClose: false }
+      {template: this.forgotPasswordTemplate, title: '¿Olvidó su clave?'},
+      {width: 376, height: 663, disableClose: false}
     );
   }
 
@@ -78,7 +71,7 @@ export class ForgotPasswordComponent implements OnInit {
 
   getIdentTypes() {
     this.httpService
-      .post("canales", "global/identification-types", {
+      .post('canales', 'global/identification-types', {
         channelId: 102,
       })
       .pipe(finalize(() => this.identificationTypeChanged()))
@@ -94,18 +87,18 @@ export class ForgotPasswordComponent implements OnInit {
     if (this.forgotPassForm.valid) {
       this.httpService
         .post(
-          "canales",
-          "security/user/forgetusernameandpasswordbyidentification",
+          'canales',
+          'security/user/forgetusernameandpasswordbyidentification',
           {
-            codeCredix: this.forgotPassForm.get("code").value,
-            typeIdentification: this.forgotPassForm.get("identType").value,
-            identification: this.forgotPassForm.get("identNumber").value,
+            codeCredix: this.forgotPassForm.get('code').value,
+            typeIdentification: this.forgotPassForm.get('identType').value,
+            identification: this.forgotPassForm.get('identNumber').value,
             channelId: 102,
             password: CryptoJS.SHA256(
-              this.forgotPassForm.get("password").value
+              this.forgotPassForm.get('password').value
             ),
             passwordConfirmation: CryptoJS.SHA256(
-              this.forgotPassForm.get("confirmPassword").value
+              this.forgotPassForm.get('confirmPassword').value
             ),
           }
         )
@@ -116,7 +109,7 @@ export class ForgotPasswordComponent implements OnInit {
         )
         .subscribe((res) => {
           this.submitted = true;
-          if ((res.type = "success")) {
+          if ((res.type = 'success')) {
             console.log(res);
             this.message = res.message;
             this.showErrorMessage = true;
@@ -141,23 +134,23 @@ export class ForgotPasswordComponent implements OnInit {
       }
 
       switch (value) {
-        case "CN": {
-          this.identMask = "0-0000-0000";
+        case 'CN': {
+          this.identMask = '0-0000-0000';
           this.identMaxLength = 9;
           break;
         }
-        case "CJ": {
-          this.identMask = "0-000-000000";
+        case 'CJ': {
+          this.identMask = '0-000-000000';
           this.identMaxLength = 10;
           break;
         }
-        case "CR": {
-          this.identMask = "000000000000";
+        case 'CR': {
+          this.identMask = '000000000000';
           this.identMaxLength = 12;
           break;
         }
-        case "PE": {
-          this.identMask = "000000000000";
+        case 'PE': {
+          this.identMask = '000000000000';
           this.identMaxLength = 12;
           break;
         }
