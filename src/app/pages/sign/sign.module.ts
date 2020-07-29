31d2cfedb72bd834d9e30a4eaab3a50b7d4fd14f
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-29 12:31:20 -0400

    Implementacion del sign-header y sign-footer en sign.

--- src/app/pages/sign/sign.module.ts
+++ src/app/pages/sign/sign.module.ts
@@ -3,15 +3,19 @@ import {CommonModule} from '@angular/common';
 import {SignComponent} from './sign.component';
 import {SignRoutingModule} from './sign-routing.module';
 import {SharedModule} from '../../shared/shared.module';
-import { DeviceDetectorModule } from 'ngx-device-detector';
+import {DeviceDetectorModule} from 'ngx-device-detector';
+import {ExtendedModule} from '@angular/flex-layout';
+import {SignHeaderComponent} from './sign-header/sign-header.component';
+import {SignFooterComponent} from './sign-footer/sign-footer.component';
 
 @NgModule({
-  declarations: [SignComponent],
+  declarations: [SignComponent, SignHeaderComponent, SignFooterComponent],
   imports: [
     CommonModule,
     SignRoutingModule,
     SharedModule,
-    DeviceDetectorModule
+    DeviceDetectorModule,
+    ExtendedModule
   ]
 })
 export class SignModule {
