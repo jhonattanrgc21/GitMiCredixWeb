7058d5eae00f0efab58ed137aaaf476517b996c7
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-30 18:48:37 -0400

    Consumo de servicio /homepage/tagshomepage en el componente landing.component.ts
    Envio de parametros a payment-details.component.ts

--- src/app/pages/home/features/landing/landing.component.ts
+++ src/app/pages/home/features/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {LandingService} from './landing.service';
 
 @Component({
   selector: 'app-landing',
@@ -6,11 +7,37 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  paymentDetails: PaymentDetails;
 
-  constructor() {
+  constructor(private landingService: LandingService) {
   }
 
   ngOnInit(): void {
+    this.landingService.getHomeContent().subscribe(response => {
+      this.paymentDetails = {
+        currentDate: response.fechaActual,
+        nextCutOffDate: response.fechaProximaCorte,
+        nextPaymentDate: response.fechaProximaMaximaPago,
+        lastCutOffDate: response.ultimaFechaCorte,
+        lastPaymentDate: response.ultimaFechaMaximoPago,
+        minPaymentColones: response.pagoMinimoActColones,
+        minPaymentDollars: response.pagoMinimoActDolares,
+        cashPaymentColones: response.pagoContadoColones,
+        cashPaymentDollars: response.pagoContadoDolares
+      };
+    });
   }
 
+}
+
+export interface PaymentDetails {
+  currentDate: string;
+  nextCutOffDate: string;
+  nextPaymentDate: string;
+  lastCutOffDate: string;
+  lastPaymentDate: string;
+  minPaymentColones: number;
+  minPaymentDollars: number;
+  cashPaymentColones: number;
+  cashPaymentDollars: number;
 }
---
+++ src/app/pages/home/features/landing/landing.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from '../../../../core/services/http.service';
+import {StorageService} from '../../../../core/services/storage.service';
+import {DatePipe} from '@angular/common';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LandingService {
+  tagsHomePageUri = 'homepage/tagshomepage';
+
+  constructor(private httpService: HttpService, private storageService: StorageService) {
+  }
+
+  getHomeContent() {
+    return this.httpService.post('canales', this.tagsHomePageUri, {
+      cardId: this.storageService.getCurrentCards().find(card => card.category === 'Principal').cardId,
+      userId: this.storageService.getCurrentUser().userId,
+      hour: new DatePipe('es').transform(new Date(), 'HH:MM')
+    })
+      .pipe(map(response => {
+        return response.json;
+      }));
+  }
+}
