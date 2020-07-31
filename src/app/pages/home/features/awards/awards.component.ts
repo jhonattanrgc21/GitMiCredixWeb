a497cc56f009c8b914abc64f5ad88126f018058f
María Alejandra Lomeli Sandia <mlomeli@intelix.ad>
2020-07-31 16:06:56 -0400

    servicio agregado

--- src/app/pages/home/features/awards/awards.component.ts
+++ src/app/pages/home/features/awards/awards.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../../../../core/services/modal.service';
 import {ModalAwardsComponent} from './modal-awards/modal-awards.component';
+import {StorageService} from '../../../../core/services/storage.service';
+import {HttpService} from '../../../../core/services/http.service';
 
 @Component({
   selector: 'app-awards',
@@ -13,10 +15,15 @@ export class AwardsComponent implements OnInit {
     { id: 2, name: 'Finalizados' },
   ];
   tab_show = 1;
+  userChallenges;
 
-  constructor(private modalService: ModalService) { }
+  constructor(private modalService: ModalService,  private storageService: StorageService, private httpServide: HttpService) { }
+
 
   ngOnInit(): void {
+
+    this.userChallenges = this.getUserChallenges();
+    console.log(this.userChallenges);
   }
 
   tabSelected(tab) {
@@ -31,6 +38,16 @@ export class AwardsComponent implements OnInit {
   open(){
     this.modalService.open({ component: ModalAwardsComponent, title: 'Premios' },
       { width: 376, height: 584, disableClose: true });
+  }
+
+  getUserChallenges(){
+    const sibUserId = this.storageService.getCurrentUser().userId;
+
+    this.httpServide.post('canales', `api/canalesbe/messagesrewards/challenges/user/${sibUserId}`, {"channelId":102, "usuId": sibUserId})
+    .subscribe((resp)=>{
+
+    })
+
   }
 
 }
