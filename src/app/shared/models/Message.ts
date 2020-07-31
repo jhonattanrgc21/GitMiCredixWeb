26f39beff24e43a9d71c9b55a7bcba6604acf48f
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-31 13:17:26 -0400

    tipo Message.ts

---
+++ src/app/shared/models/Message.ts
@@ -0,0 +1,13 @@
+export interface Message {
+  id: number;
+  previewImage: string;
+  image: string;
+  title: string;
+  text: string;
+  status: string;
+  sentDate: string;
+  creationDate: string;
+  expirationDate: string;
+  readDate: string;
+  type: string;
+}
