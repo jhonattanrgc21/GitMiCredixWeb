4ba496058bd9a47741d726f3c4750fdd0934adc5
María Alejandra Lomeli Sandia <mlomeli@intelix.ad>
2020-08-03 13:54:18 -0400

    pruebas unitarias

--- src/app/pages/home/features/payment-places/payment-places.component.spec.ts
+++ src/app/pages/home/features/payment-places/payment-places.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 import { PaymentPlacesComponent } from './payment-places.component';
+import {HttpService} from 'src/app/core/services/http.service';
 
 describe('PaymentPlacesComponent', () => {
   let component: PaymentPlacesComponent;
   let fixture: ComponentFixture<PaymentPlacesComponent>;
+  let element: HTMLElement;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentPlacesComponent ]
+      declarations: [ PaymentPlacesComponent ],
+      providers: [HttpService]
     })
     .compileComponents();
   }));
@@ -22,4 +28,17 @@ describe('PaymentPlacesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display places by default', async(() => {
+    element = fixture.debugElement.query(By.css('.navigation-table')).nativeElement;
+    expect(element.querySelector('.navigation-table__content')).toBeTruthy();
+  }));
+
+  it('should display digital patyments after clicking second tab', async(() => {
+    element = fixture.debugElement.query(By.css('.credix-tab__label .ng-star-inserted')).nativeElement;
+    element.click();
+    expect(element.querySelector('.digital-payments__content')).toBeTruthy();
+  }));
+
+
 });
