51e1c85c9720d7970085606914af23bae18fd3c1
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-08-04 15:29:57 -0400

    Directiva para campos de texto.

---
+++ src/app/shared/directives/credix-forms/credix-form-field.directive.ts
@@ -0,0 +1,103 @@
+import {AfterViewInit, Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[credixFormField]'
+})
+export class CredixFormFieldDirective implements AfterViewInit {
+  @Input() onFocusLabel: string;
+  @Input() height: number;
+  label: string;
+  inputClass = '.mat-input-element';
+  labelWrapperClass = '.mat-form-field-label-wrapper';
+  labelClass = '.mat-form-field-label';
+  fieldFlexClass = '.mat-form-field-flex';
+  fieldInfixClass = '.mat-form-field-infix';
+  fieldUnderlineClass = '.mat-form-field-underline';
+  fieldSuffixClass = '.mat-form-field-suffix';
+  fieldRippleClass = '.mat-form-field-ripple';
+  iconClass = '.mat-icon';
+  errorClass = '.mat-error';
+  inputEl: any;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.inputEl = this.el.nativeElement.querySelector(this.inputClass);
+    this.label = this.el.nativeElement.querySelector(this.labelClass).children[0].innerHTML;
+
+    if (this.height) {
+      this.renderer.setStyle(this.el.nativeElement, 'height', `${this.height}px`);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldFlexClass), 'height', `${this.height}px`);
+    }
+
+    this.renderer.setAttribute(this.inputEl, 'style',
+      'padding-left: 8px; font-weight: bold; font-size: 16px; height: 24px; vertical-align: sub');
+
+    this.renderer.setAttribute(this.el.nativeElement.querySelector(this.labelClass), 'style',
+      'font-size: 16px; color: #3e3e3e; text-align: left; margin-left: 8px; width: 100%; height: 18px');
+
+    this.renderer.setAttribute(this.el.nativeElement.querySelector(this.fieldFlexClass), 'style',
+      'padding: 0');
+
+    this.renderer.setAttribute(this.el.nativeElement.querySelector(this.fieldInfixClass), 'style',
+      `padding-bottom: 16px; padding-top: 4px; border: 0; height: ${this.height}px`);
+
+    this.renderer.setAttribute(this.el.nativeElement.querySelector(this.fieldUnderlineClass), 'style',
+      'background: #C7C7C7');
+
+    if (this.el.nativeElement.classList.contains('mat-form-field-disabled')) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelClass), 'color', '#D2D2D2', 1);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldUnderlineClass), 'background', '#D2D2D2', 1);
+    }
+  }
+
+  @HostListener('focusin') onFocusIn() {
+    if (this.onFocusLabel) {
+      this.el.nativeElement.querySelector(this.labelClass).children[0].innerHTML = this.onFocusLabel;
+    }
+
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldInfixClass), 'padding-bottom', '6px', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldInfixClass), 'padding-top', '0', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelWrapperClass), 'padding-top', '0', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelWrapperClass), 'top', '0', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.inputClass), 'margin-top', '16px', 1);
+
+
+    if (this.el.nativeElement.classList.contains('mat-form-field-invalid')) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelClass), 'color', '#FF4965', 1);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldRippleClass), 'background-color', '#FF4965', 1);
+    } else {
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelClass), 'color', '#3e3e3e', 1);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldRippleClass), 'background-color', '#707070', 1);
+    }
+  }
+
+  @HostListener('focusout') onFocusOut() {
+    if (this.onFocusLabel) {
+      this.el.nativeElement.querySelector(this.labelClass).children[0].innerHTML = this.inputEl.value ? this.onFocusLabel : this.label;
+    }
+
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldInfixClass), 'padding-bottom',
+      this.inputEl.value ? '6px' : '16px', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldInfixClass), 'padding-top',
+      this.inputEl.value ? '0' : '4px', 1);
+    this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelWrapperClass), 'top',
+      this.inputEl.value ? '0' : '-16px', 1);
+
+    if (this.el.nativeElement.classList.contains('mat-form-field-invalid')) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelClass), 'color', '#FF4965', 1);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldRippleClass), 'background-color', '#FF4965', 1);
+    } else {
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.labelClass), 'color', '#3e3e3e', 1);
+      this.renderer.setStyle(this.el.nativeElement.querySelector(this.fieldRippleClass), 'background-color', '#707070', 1);
+    }
+  }
+
+  @HostListener('keyup')
+  onKeyUp() {
+
+  }
+}
