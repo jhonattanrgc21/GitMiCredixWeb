48605488d80fdcaeb4196fa70005a5cbc03c81e9
Emmanuel Ortega <eortega@intelix.biz>
2020-08-03 21:13:09 -0400

    cambios con consumo de servicios en marchamos

--- src/app/pages/home/features/marchamos/marchamos.component.ts
+++ src/app/pages/home/features/marchamos/marchamos.component.ts
@@ -6,6 +6,8 @@ import {VehicleType} from 'src/app/shared/models/vehicleType.models';
 import {CdkStepper} from '@angular/cdk/stepper';
 import {MatDialogRef} from '@angular/material/dialog';
 import {PopupMarchamosDetailComponent} from './popup-marchamos-detail/popup-marchamos-detail.component';
+import { ConsultVehicle } from 'src/app/shared/models/consultVehicle.models';
+import { Item } from 'src/app/shared/models/item.model';
 
 @Component({
   selector: 'app-marchamos',
@@ -14,180 +16,9 @@ import {PopupMarchamosDetailComponent} from './popup-marchamos-detail/popup-marc
 })
 export class MarchamosComponent implements OnInit {
 
-  vehicleType: VehicleType[] = [
-    {
-      description: 'ALAJUELA BUS',
-      type: 1
-    },
-    {
-      description: 'ALAJUELA PUBLICO',
-      type: 2
-    },
-    {
-      description: 'BICIMOTOS',
-      type: 3
-    },
-    {
-      description: 'CARGA PESADA',
-      type: 4
-    },
-    {
-      description: 'CARTAGO BUS',
-      type: 5
-    },
-    {
-      description: 'CARGA LIVIANA',
-      type: 6
-    },
-    {
-      description: 'CARTAGO PUBLICO',
-      type: 7
-    },
-    {
-      description: 'CRUZ ROJA COSTARRICENSE',
-      type: 8
-    },
-    {
-      description: 'DISCAPACITADO',
-      type: 9
-    },
-    {
-      description: 'EQUIPO ESPECIAL',
-      type: 10
-    },
-    {
-      description: 'EXPORTACION',
-      type: 11
-    },
-    {
-      description: 'GUANACASTE BUS',
-      type: 12
-    },
-    {
-      description: 'GUANACASTE PUBLICO',
-      type: 13
-    },
-    {
-      description: 'HEREDIA BUS',
-      type: 14
-    },
-    {
-      description: 'HEREDIA PUBLICO',
-      type: 15
-    },
-    {
-      description: 'LIMON BUS',
-      type: 16
-    },
-    {
-      description: 'LIMITACIONES FISICAS PERMANENTES',
-      type: 17
-    },
-    {
-      description: 'LIMON PUBLICO',
-      type: 18
-    },
-    {
-      description: 'MUDIAL DE FUTBOL FEMENINO 2014',
-      type: 19
-    },
-    {
-      description: 'MOTOS',
-      type: 20
-    },
-    {
-      description: 'PARTICULAR',
-      type: 21
-    },
-    {
-      description: 'PUNTARENAS BUS',
-      type: 22
-    },
-    {
-      description: 'PENSIONADO',
-      type: 23
-    },
-    {
-      description: 'PUNTARENAS PUBLICO',
-      type: 24
-    },
-    {
-      description: 'SAN JOSE BUS',
-      type: 25
-    },
-    {
-      description: 'SAN JOSE PUBLICO',
-      type: 26
-    },
-    {
-      description: 'TAXI ALAJUELA',
-      type: 27
-    },
-    {
-      description: 'AEROPUERTO INTERNACIONAL JUAN SANTAMARIA',
-      type: 28
-    },
-    {
-      description: 'PERMISO DE TAXI',
-      type: 29
-    },
-    {
-      description: 'TAXI CARTAGO',
-      type: 30
-    },
-    {
-      description: 'TAXI GUANACASTE',
-      type: 31
-    },
-    {
-      description: 'TAXI HEREDIA',
-      type: 32
-    },
-    {
-      description: 'TAXI LIMON',
-      type: 33
-    },
-    {
-      description: 'TAXI PUNTARENAS',
-      type: 34
-    },
-    {
-      description: 'TAXI SAN JOSE',
-      type: 35
-    },
-    {
-      description: 'TURISMO',
-      type: 36
-    },
-    {
-      description: 'CLASE TEMPORAL PARA EL PERIODO 2016',
-      type: 37
-    },
-    {
-      description: 'CLASE TEMPORAL PARA EL PERIODO 2017',
-      type: 38
-    },
-    {
-      description: 'CLASE TEMPORAL PARA EL PERIODO 2018',
-      type: 39
-    },
-    {
-      description: 'EMBAJADA DE ESTADOS UNIDOS',
-      type: 40
-    },
-    {
-      description: 'VEHICULO ESPECIAL',
-      type: 41
-    },
-    {
-      description: 'VEHICULO HISTORICO',
-      type: 42
-    },
-    {
-      description: 'ZONAS FRANCAS DE EXPORTACION',
-      type: 43
-    }
-  ];
+  vehicleType: VehicleType[];
+  consultVehicle: ConsultVehicle;
+  itemProduct: Item[];
   wishPayFirstCouteIn: any[] = [
     {
       description: 'Enero 2020',
@@ -268,6 +99,7 @@ export class MarchamosComponent implements OnInit {
 
 
   @ViewChild('stepper') stepper: CdkStepper;
+  @ViewChild('checkBoxValue') checkBoxValue: any;
 
   constructor(
     private httpService: HttpService,
@@ -296,16 +128,7 @@ export class MarchamosComponent implements OnInit {
     this.getVehicleType();
   }
 
-  consult() {
-    this.httpService.post('marchamos', 'pay/vehicleconsult', {
-      channelId: 107,
-      plateClassId: this.consultControls.vehicleType.value.toString(),
-      plateNumber: this.consultControls.plateNumber.value.toUpperCase(),
-      aditionalProducts: []
-    })
-      .subscribe(response => console.log(response));
-    this.vehicleInformation = true;
-  }
+  
 
   continue() {
     this.stepper.next();
@@ -320,12 +143,19 @@ export class MarchamosComponent implements OnInit {
     this.value = event;
   }
 
-  getValueCheckBoxes(event: any) {
+  getValueCheckBoxes(event: any, all?:string) {
     console.log(event);
+    console.log(this.checkBoxValue);
     const checkArray: FormArray = this.secureAndQuotesForm.get('aditionalProducts') as FormArray;
+    if (all === 'all') {
+      this.allChecked(event.checked);  
+    }
+    
 
     if (event.checked) {
-      checkArray.push(new FormControl(event.source.value));
+      checkArray.push(new FormGroup({
+        productCode:new FormControl(event.source.value)
+      }));
     } else {
       let index = 0;
       checkArray.controls.forEach((item: FormControl) => {
@@ -338,8 +168,31 @@ export class MarchamosComponent implements OnInit {
     }
   }
 
+  getRadioButtonsChecked(event){
+    console.log(event);
+  }
+
+  consult() {
+    this.httpService.post('marchamos', 'pay/vehicleconsult', {
+      channelId: 107,
+      plateClassId: this.consultControls.vehicleType.value.toString(),
+      plateNumber: this.consultControls.plateNumber.value.toUpperCase(),
+      aditionalProducts: []
+    })
+      .subscribe(response => { 
+        console.log(response);
+          this.consultVehicle = response.REQUESTRESULT.soaResultVehicleConsult.header;
+          this.itemProduct = response.REQUESTRESULT.soaResultVehicleConsult.item;
+        (response.type === 'success') ? this.vehicleInformation = !this.vehicleInformation : this.vehicleInformation;
+      });
+    
+  }
+
   getVehicleType() {
-    this.httpService.post('marchamos', 'pay/platetypes', {channelId: 102}).subscribe(response => console.log(response));
+    this.httpService.post('marchamos', 'pay/platetypes', {channelId: 102})
+    .subscribe(response => {
+        this.vehicleType = response.plateTypesList;
+    });
   }
 
   secureToPay() {
---
+++ src/app/shared/models/item.model.ts
@@ -0,0 +1,8 @@
+export interface Item {
+    itemCurrentAmount: number,
+    itemDescription: string,
+    itemAmount: number,
+    itemPayCode: number,
+    itemPreviousAmount: number,
+    itemAjustAmount: number
+}
