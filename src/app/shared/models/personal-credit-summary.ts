4a687610930a65cd1cad285449491186bc910931
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-08-11 16:42:33 -0400

    Tipo de datos: account-summary con informacion de disponible, cosumido y limite de tarjeta y personal-credit-summary con informacion de credito personal.

---
+++ src/app/shared/models/personal-credit-summary.ts
@@ -0,0 +1,14 @@
+export interface PersonalCreditSummary {
+  id: number;
+  AnnualPercentageInterest: string;
+  commission: string;
+  interest: string;
+  interestSecure: string;
+  percentageCommission: number;
+  percentageInterest: string;
+  quota: string;
+  secure: string;
+  term: number;
+  totalPay: string;
+  totalCredit: string;
+}
