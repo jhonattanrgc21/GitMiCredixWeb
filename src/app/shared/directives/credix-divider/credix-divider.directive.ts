b2aa5c078b19c12db82cc1888ee55928786768db
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-08-24 16:37:10 -0400

    Directiva credixDivider para agregar estilos a las lineas divisorias horizontales.

---
+++ src/app/shared/directives/credix-divider/credix-divider.directive.ts
@@ -0,0 +1,16 @@
+import {AfterViewInit, Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[credixDivider]'
+})
+export class CredixDividerDirective implements AfterViewInit {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid #E9E9E9', 1);
+    this.renderer.setStyle(this.el.nativeElement, 'width', '100%');
+  }
+}
