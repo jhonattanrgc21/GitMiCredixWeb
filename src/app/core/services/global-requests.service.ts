621b309a85175a0f0c2665cbc3afd43c36678824
María Alejandra Lomeli Sandia <mlomeli@intelix.ad>
2020-08-27 11:50:50 -0400

    cambios en global requests

--- src/app/core/services/global-requests.service.ts
+++ src/app/core/services/global-requests.service.ts
@@ -1,11 +1,11 @@
-import {Injectable} from '@angular/core';
-import {HttpService} from './http.service';
-import {map, publishReplay, refCount} from 'rxjs/operators';
-import {StorageService} from './storage.service';
-import {Observable} from 'rxjs';
-import {AccountSummary} from '../../shared/models/account-summary';
-import {Currency} from '../../shared/models/currency';
-import {IdentificationType} from '../../shared/models/IdentificationType';
+import { Injectable } from "@angular/core";
+import { HttpService } from "./http.service";
+import { map, publishReplay, refCount } from "rxjs/operators";
+import { StorageService } from "./storage.service";
+import { Observable } from "rxjs";
+import { AccountSummary } from "../../shared/models/account-summary";
+import { Currency } from "../../shared/models/currency";
+import { IdentificationType } from "../../shared/models/IdentificationType";
 
 @Injectable()
 export class GlobalRequestsService {
@@ -13,29 +13,33 @@ export class GlobalRequestsService {
   identificationTypes: Observable<IdentificationType[]>;
   accountSummary: Observable<AccountSummary>;
   quotas: Observable<number[]>;
-  private currenciesUri = 'global/currencies';
-  private identificationTypesUri = 'global/identification-types';
-  private accountSummaryUri = 'channels/accountsummary';
-  private getQuotasUri = 'customerservice/listquotabyproduct';
-  private getIbanAccountUri = 'account/getibanaccount';
+  ibanAccount: Observable<any[]>;
+  private currenciesUri = "global/currencies";
+  private identificationTypesUri = "global/identification-types";
+  private accountSummaryUri = "channels/accountsummary";
+  private getQuotasUri = "customerservice/listquotabyproduct";
+  private getIbanAccountUri = "account/getibanaccount";
 
-  constructor(private httpService: HttpService,
-              private storageService: StorageService) {
-  }
+  constructor(
+    private httpService: HttpService,
+    private storageService: StorageService
+  ) {}
 
   getCurrencies(): Observable<Currency[]> {
     if (!this.currencies) {
-      this.currencies = this.httpService.post('canales', this.currenciesUri)
+      this.currencies = this.httpService
+        .post("canales", this.currenciesUri)
         .pipe(
           publishReplay(1),
           refCount(),
-          map(response => {
-            if (response.type === 'success') {
+          map((response) => {
+            if (response.type === "success") {
               return response.currencies;
             } else {
               return [];
             }
-          }));
+          })
+        );
     }
 
     return this.currencies;
@@ -43,47 +47,54 @@ export class GlobalRequestsService {
 
   getIdentificationTypes() {
     if (!this.identificationTypes) {
-      this.identificationTypes = this.httpService.post('canales', this.identificationTypesUri)
+      this.identificationTypes = this.httpService
+        .post("canales", this.identificationTypesUri)
         .pipe(
           publishReplay(1),
           refCount(),
-          map(response => {
-            if (response.type === 'success') {
+          map((response) => {
+            if (response.type === "success") {
               return response.identificationTypes;
             } else {
               return [];
             }
-          }));
+          })
+        );
     }
 
     return this.identificationTypes;
   }
 
-  getAccountSummary(cardId: number = this.storageService.getCurrentCards().find(card => card.category === 'Principal').cardId):
-    Observable<AccountSummary> {
+  getAccountSummary(
+    cardId: number = this.storageService
+      .getCurrentCards()
+      .find((card) => card.category === "Principal").cardId
+  ): Observable<AccountSummary> {
     if (!this.accountSummary) {
-      this.accountSummary = this.httpService.post('canales', this.accountSummaryUri, {
-        cardId,
-        userId: this.storageService.getCurrentUser().userId
-      })
+      this.accountSummary = this.httpService
+        .post("canales", this.accountSummaryUri, {
+          cardId,
+          userId: this.storageService.getCurrentUser().userId,
+        })
         .pipe(
           publishReplay(1),
           refCount(),
-          map(response => {
-            if (response.type === 'success' || response.titleOne === 'Éxito') {
+          map((response) => {
+            if (response.type === "success" || response.titleOne === "Éxito") {
               return {
                 available: response.json.compracuotasdisp,
                 limit: response.json.compra,
-                consumed: response.json.consumed
+                consumed: response.json.consumed,
               };
             } else {
               return {
-                available: '0',
-                limit: '0',
-                consumed: '0'
+                available: "0",
+                limit: "0",
+                consumed: "0",
               };
             }
-          }));
+          })
+        );
     }
 
     return this.accountSummary;
@@ -91,26 +102,31 @@ export class GlobalRequestsService {
 
   getQuotas(productId: number): Observable<number[]> {
     if (!this.quotas) {
-      this.quotas = this.httpService.post('canales', this.getQuotasUri, {
-        productId
-      })
+      this.quotas = this.httpService
+        .post("canales", this.getQuotasUri, {
+          productId,
+        })
         .pipe(
           publishReplay(1),
           refCount(),
-          map(response => {
-            if (response.type === 'success') {
-              return response.listQuota.map(q => q.quota);
+          map((response) => {
+            if (response.type === "success") {
+              return response.listQuota.map((q) => q.quota);
             } else {
               return [];
             }
-          }));
+          })
+        );
     }
 
     return this.quotas;
   }
 
   getIbanAccount() {
-    return this.httpService.post('canales', this.getIbanAccountUri, {channelId: 102});
+    if (!this.ibanAccount) {
+      return this.httpService
+        .post("canales", this.getIbanAccountUri, { channelId: 102 })
+        .pipe(publishReplay(1), refCount());
+    }
   }
-
 }
