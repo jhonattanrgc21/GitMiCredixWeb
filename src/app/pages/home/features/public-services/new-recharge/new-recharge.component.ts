01edf1b6a54bd84d3ed32c4717a25583ba2e20fb
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-24 12:54:41 -0400

    Diseno de recargas.

--- src/app/pages/home/features/public-services/new-recharge/new-recharge.component.ts
+++ src/app/pages/home/features/public-services/new-recharge/new-recharge.component.ts
@@ -1,5 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PendingReceipts} from '../../../../../shared/models/pending-receipts';
+import {finalize} from 'rxjs/operators';
+import {ConvertStringDateToDate, getMontByMonthNumber} from '../../../../../shared/utils';
+import {PublicServicesService} from '../public-services.service';
+import {PublicServicesApiService} from '../../../../../core/services/public-services-api.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ModalService} from '../../../../../core/services/modal.service';
 
 @Component({
   selector: 'app-new-recharge',
@@ -7,14 +14,117 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./new-recharge.component.scss']
 })
 export class NewRechargeComponent implements OnInit {
+  rechargeFormGroup: FormGroup = new FormGroup({
+    phoneNumber: new FormControl(null, [Validators.required]),
+    amount: new FormControl(null, [Validators.required]),
+    credixCode: new FormControl(null, [Validators.required]),
+  });
+  favoriteControl = new FormControl(null);
+  amounts = [
+    '1.000,00',
+    '2.000,00',
+    '5.000,00',
+    '10.000,00',
+    'Otro',
+  ];
+  anotherAmount = false;
+  saveAsFavorite = false;
+  done = false;
+  hasReceipts = true;
+  pendingReceipts: PendingReceipts;
+  publicServiceId: number;
+  referenceName: string;
+  name: string;
+  month: string;
+  expirationDate: Date;
+  title: string;
+  message: string;
+  status: 'success' | 'error';
+  today = new Date();
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private publicServicesService: PublicServicesService,
+              private publicServicesApiService: PublicServicesApiService,
+              private router: Router,
+              private modalService: ModalService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      console.log(params.id);
+      this.publicServiceId = +params.serviceId;
+      this.getEnterprise(+params.categoryId, +params.enterpriseId);
+      this.publicServicesService.getReferenceName(+params.categoryId).subscribe(referenceName => this.referenceName = referenceName);
     });
+  }
+
+  getEnterprise(categoryId: number, enterpriseId: number) {
+    this.publicServicesApiService.getPublicServiceEnterpriseByCategory(categoryId).subscribe(publicServiceEnterprises =>
+      this.title = publicServiceEnterprises
+        .find(enterprise => enterprise.publicServiceEnterpriseId === enterpriseId).publicServiceEnterpriseDescription);
+  }
+
+  onCheckboxChanged(checked: boolean) {
+    this.saveAsFavorite = checked;
+    if (this.saveAsFavorite) {
+      this.favoriteControl.setValidators([Validators.required]);
+    }
+  }
+
+  onAmountChanged(value) {
+    if (value !== 'Otro') {
+      this.anotherAmount = false;
+      this.rechargeFormGroup.controls.amount.setValue(value);
+    } else {
+      this.anotherAmount = true;
+    }
+  }
+
+  openModal() {
+    this.modalService.confirmationPopup('¿Desea realizar esta recarga?').subscribe(confirmation => {
+      if (confirmation) {
+        const receipt = this.pendingReceipts.receipts[0];
+        this.publicServicesService.payPublicService(
+          this.publicServiceId,
+          +receipt.serviceValue,
+          receipt.totalAmount,
+          +receipt.receiptPeriod,
+          receipt.expirationDate,
+          receipt.billNumber)
+          .pipe(finalize(() => this.done = true))
+          .subscribe(response => {
+            this.status = response.type;
+            this.message = response.responseDescription || response.message;
+            if (response.type === 'success' && this.saveAsFavorite) {
+              this.saveFavorite();
+            }
+          });
+      }
+    });
+  }
+
+  saveFavorite() {
+    this.publicServicesService.savePublicServiceFavorite(
+      this.publicServiceId,
+      this.rechargeFormGroup.controls.phoneNumber.value,
+      this.favoriteControl.value,
+      this.rechargeFormGroup.controls.credixCode.value).subscribe();
+  }
+
+  back() {
+    this.router.navigate(['/home/public-services']);
+  }
+
+  checkPendingReceipts() {
+    this.publicServicesService.checkPendingReceipts(this.publicServiceId, this.rechargeFormGroup.controls.phoneNumber.value)
+      .subscribe(pendingReceipts => {
+        if (pendingReceipts.receipts) {
+          this.pendingReceipts = pendingReceipts;
+          this.month = getMontByMonthNumber(ConvertStringDateToDate(pendingReceipts.receipts[0].receiptPeriod).getMonth());
+          this.expirationDate = ConvertStringDateToDate(pendingReceipts.receipts[0].expirationDate);
+        } else {
+          this.hasReceipts = false;
+        }
+      });
   }
 
 }
