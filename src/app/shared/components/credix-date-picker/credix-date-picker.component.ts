4976ae98686dd99de9e3eecb90222e79d8f11d22
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-28 17:41:59 -0400

    Nuevo componente: date picker, basado en controlValueAccesor.

---
+++ src/app/shared/components/credix-date-picker/credix-date-picker.component.ts
@@ -0,0 +1,62 @@
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ModalService} from '../../../core/services/modal.service';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'credix-date-picker',
+  templateUrl: './credix-date-picker.component.html',
+  styleUrls: ['./credix-date-picker.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CredixDatePickerComponent),
+      multi: true
+    }
+  ]
+})
+export class CredixDatePickerComponent implements OnInit, ControlValueAccessor {
+  @Input() label: string;
+  control = new FormControl(null);
+
+  constructor(private modalService: ModalService) {
+  }
+
+  ngOnInit(): void {
+    this.control.valueChanges.subscribe(value => this.propagateChange(value));
+  }
+
+  openCalendar() {
+    this.modalService.openCalendarPopup().subscribe(modal => {
+      if (modal) {
+        this.control.setValue(modal.date);
+      }
+    });
+  }
+
+  onTouch() {
+    this.propagateTouch();
+  }
+
+  writeValue(value: any): void {
+    this.control.setValue(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.control.disable() : this.control.enable();
+  }
+
+  propagateChange = (_: any) => {
+  };
+
+  propagateTouch = () => {
+  };
+}
