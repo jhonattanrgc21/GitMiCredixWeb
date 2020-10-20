3bd8bbbb90dd90aeddbf238f088315835979ab48
Andres Reyes <andresajrb@gmail.com>
2020-10-20 08:36:58 -0400

    feat: Se migro la gention de los estilos a ngClass

--- src/app/shared/components/credix-search-box/credix-search-box.component.ts
+++ src/app/shared/components/credix-search-box/credix-search-box.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component, Input, OnInit, HostListener, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,25 +8,9 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./credix-search-box.component.scss']
 })
 export class CredixSearchBoxComponent implements OnInit {
-  @Input() data: { title: string; description: string; icon: string; result: any }[];
-  @Output() selectionEvent: EventEmitter<any> = new EventEmitter<any>();
-  COMPONENT_CLASSES: any = {
-    SEARCH_BOX: {
-      PRINCIPAL: 'search_box',
-      ACTIVE: 'search_box_active'
-    },
-    SEARCH_INPUT_TEXT: {
-      PRINCIPAL: 'search_input_text',
-      ACTIVE: 'search_input_text_active'
-    },
-    SEARCH_PANEL: {
-      PRINCIPAL: 'search_panel_inactive',
-      ACTIVE: 'search_panel'
-    }
-  };
-  searchBoxStyle = 'search_box';
-  searchInputStyle = 'search_input_text';
-  searchPanelStyle = 'search_panel_inactive';
+  @Input() initialObject: any;
+  @Output() returnResponse: EventEmitter<any> = new EventEmitter<any>();
+  searchPanelStyle: boolean = false;
   inputControl = new FormControl();
   searchingResult: any = [];
 
@@ -41,35 +25,29 @@ export class CredixSearchBoxComponent implements OnInit {
   @HostListener('focusout')
   onFocusout() {
     setTimeout(() => {
-      this.searchBoxStyle = this.COMPONENT_CLASSES.SEARCH_BOX.PRINCIPAL;
-      this.searchInputStyle = this.COMPONENT_CLASSES.SEARCH_INPUT_TEXT.PRINCIPAL;
-      this.searchPanelStyle = this.COMPONENT_CLASSES.SEARCH_PANEL.PRINCIPAL;
+      this.searchPanelStyle = false;
       this.inputControl.reset();
     }, 300);
   }
 
   onClickItem(item: any) {
-    this.selectionEvent.emit(item);
+    this.returnResponse.emit(item);
   }
 
   onInputChanged() {
     this.inputControl.valueChanges.subscribe(value => {
       setTimeout(() => {
         if (value) {
-          this.searchBoxStyle = this.COMPONENT_CLASSES.SEARCH_BOX.ACTIVE;
-          this.searchInputStyle = this.COMPONENT_CLASSES.SEARCH_INPUT_TEXT.ACTIVE;
-          this.searchPanelStyle = this.COMPONENT_CLASSES.SEARCH_PANEL.PRINCIPAL;
+          this.searchPanelStyle = false;
           const input = value.toLowerCase();
-          this.searchingResult = this.data.filter(element => {
+          this.searchingResult = this.initialObject.filter(element => {
             return (element.title.toLowerCase().includes(input) || element.description.toLowerCase().includes(input));
           });
           if (this.searchingResult.length > 0) {
-            this.searchPanelStyle = this.COMPONENT_CLASSES.SEARCH_PANEL.ACTIVE;
+            this.searchPanelStyle = true;
           }
         } else {
-          this.searchBoxStyle = this.COMPONENT_CLASSES.SEARCH_BOX.PRINCIPAL;
-          this.searchInputStyle = this.COMPONENT_CLASSES.SEARCH_INPUT_TEXT.PRINCIPAL;
-          this.searchPanelStyle = this.COMPONENT_CLASSES.SEARCH_PANEL.PRINCIPAL;
+          this.searchPanelStyle = false;
         }
       }, 300);
     });
