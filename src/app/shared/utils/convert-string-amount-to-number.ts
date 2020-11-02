d40220040ff4575388b1cac6a7a90803a449473d
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-11-02 14:00:38 -0400

    fix(credix-numeric-block): Revision de caracter separador de decimales
    - Se verifica si el separador decimal es ","
    - En caso de ser "," el separador decimal se remplazan "." (si los hay) por espacios vacios "" y la "," por "." para luego convertir a numerico.
    - En caso de ser "." el separador decimal, se extraen todos los puntos y se multiplica y divide entre 100 para generar el valor numerico.

--- src/app/shared/utils/convert-string-amount-to-number.ts
+++ src/app/shared/utils/convert-string-amount-to-number.ts
@@ -1,6 +1,9 @@
 export function ConvertStringAmountToNumber(value: string): number {
-  const separator = value.indexOf(',') > -1 ? ',' : '.';
-  const integerValue = value.split(separator)[0].replace(/\./g, '');
-  const decimalValue = value.split(separator)[1] ? value.split(separator)[1].substring(0, value.split(separator)[1].length) : '00';
-  return Number(`${integerValue}.${decimalValue ? decimalValue : '00'}`);
+  if (value.indexOf(',') > -1) {
+    const numberValue = value.replace(/\./g, '').replace(',', '.');
+    return Number(numberValue);
+  } else {
+    const numberValue = value.replace(/\./g, '');
+    return (Number(numberValue) * 100) / 100;
+  }
 }
