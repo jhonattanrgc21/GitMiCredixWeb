1faab41c6d6c537dc3204e42e7100b119fcb5b0a
Daniel Stefanelli <dsteffani@cdlac.com>
2020-12-08 15:43:28 -0400

    cambios revertidos.

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   url: 'https://qapersonas.credix.com/',
-  urlCanales: 'https://canalesbe.credix.com/api/canalesbe/',
+  urlCanales: 'https://qacanalesbe.credix.com/api/canalesbe/',
   urlMarchamos: 'https://qamarchamoscore.credix.com/',
-  urlIncomex: 'https://incomexbe.credix.com/incomex/',
+  urlIncomex: 'https://qaincomexbe.credix.com/incomex/',
   channelId: 102
 };
