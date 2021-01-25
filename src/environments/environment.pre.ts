c48072aae0d0c4958e3fc1faec1c89565d7289c4
Daniel Stefanelli <dsteffani@cdlac.com>
2021-01-25 11:17:10 -0400

    build(environments): Agregar canal 110 para enviar en caso de que el inicio de sesion provenga del bot.

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -4,5 +4,6 @@ export const environment = {
   urlCanales: 'https://precanalesbe.credix.com/api/canalesbe/',
   urlMarchamos: 'https://premarchamoscore.credix.com/',
   urlIncomex: 'https://preincomexbe.credix.com/incomex/',
-  channelId: 102
+  channelId: 102,
+  channelIdFromBot: 110
 };
