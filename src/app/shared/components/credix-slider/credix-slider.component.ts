81c4d7fa038e28e7a48bd7f16058ab7633b0708f
lpena <lpena@credix.com>
2021-10-22 12:00:17 -0400

    Actualizar credix-slider.component.ts

--- src/app/shared/components/credix-slider/credix-slider.component.ts
+++ src/app/shared/components/credix-slider/credix-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {MatSlider} from '@angular/material/slider';
 
 @Component({
