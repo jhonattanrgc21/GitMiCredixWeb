fc6d5c7f39213b253184a77652ede2d5bff417b8
mriera <mriera@intelix.biz>
2024-04-01 09:59:18 -0400

    479 - Correccion fecha popup cancelar subscripcion

--- src/app/pages/home/features/credix-mas/credix-mas-account-info/credix-mas-account-info.component.ts
+++ src/app/pages/home/features/credix-mas/credix-mas-account-info/credix-mas-account-info.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalService } from 'src/app/core/services/modal.service';
-import { SubscribePopupComponent } from './subscribe-popup/subscribe-popup.component';
-import { SlowBuffer } from 'buffer';
-import { Router } from '@angular/router';
-import { CredixMasService } from '../credix-mas.service';
-import { AccountInfo } from 'src/app/shared/models/credix-mas-info';
+import { Component, OnInit } from "@angular/core";
+import { ModalService } from "src/app/core/services/modal.service";
+import { SubscribePopupComponent } from "./subscribe-popup/subscribe-popup.component";
+import { SlowBuffer } from "buffer";
+import { Router } from "@angular/router";
+import { CredixMasService } from "../credix-mas.service";
+import { AccountInfo } from "src/app/shared/models/credix-mas-info";
 
 @Component({
-  selector: 'app-credix-mas-account-info',
-  templateUrl: './credix-mas-account-info.component.html',
-  styleUrls: ['./credix-mas-account-info.component.scss']
+  selector: "app-credix-mas-account-info",
+  templateUrl: "./credix-mas-account-info.component.html",
+  styleUrls: ["./credix-mas-account-info.component.scss"],
 })
 export class CredixMasAccountInfoComponent implements OnInit {
-
   subscription = false;
   info: AccountInfo = {} as AccountInfo;
   today = new Date();
   chargeDay: number;
 
-  constructor(private modalService: ModalService,
+  constructor(
+    private modalService: ModalService,
     private router: Router,
-    private credixMasService: CredixMasService) { }
+    private credixMasService: CredixMasService
+  ) {}
 
   ngOnInit(): void {
     this.subscription = this.credixMasService.subscription;
@@ -28,32 +29,48 @@ export class CredixMasAccountInfoComponent implements OnInit {
     this.chargeDay = this.getChargeDay();
   }
 
-  getChargeDay(){
-    if(this.subscription){
+  getChargeDay() {
+    if (this.subscription) {
       return Number(this.info.activationDate.split("/")[0]);
-    }else{
+    } else {
       let someDate = new Date();
       let result = someDate.setDate(someDate.getDate() + 2);
       return new Date(result).getDate();
     }
   }
 
-  subscribe(){
+  subscribe() {
+    let date = new Date();
+    date.setDate(date.getDate() + 2);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    let currentDate = `${day}/${month}/${year}`;
     this.modalService
       .open(
-        { data: {
+        {
+          data: {
             subscription: this.subscription,
-            date: '27/08/2023'
-          }, hideCloseButton: true, component: SubscribePopupComponent},
-        {width: 400, height: 229, disableClose: false, panelClass: 'promo-popup'}, 1)
-        .afterClosed().subscribe((value) => {
-          if(value){
-            this.credixMasService.subscribe().subscribe(() => {
-              this.router.navigate(['/home/credix-mas/success'])
-            })
-          }
-        });
+            date: currentDate,
+          },
+          hideCloseButton: true,
+          component: SubscribePopupComponent,
+        },
+        {
+          width: 400,
+          height: 229,
+          disableClose: false,
+          panelClass: "promo-popup",
+        },
+        1
+      )
+      .afterClosed()
+      .subscribe((value) => {
+        if (value) {
+          this.credixMasService.subscribe().subscribe(() => {
+            this.router.navigate(["/home/credix-mas/success"]);
+          });
+        }
+      });
   }
-
-
 }
