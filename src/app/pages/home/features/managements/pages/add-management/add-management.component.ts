56a046c36a452f872c8001739379cd4632d42341
Randy Naranjo <randy.naranjo@centaurosolutions.com>
2024-08-26 12:26:32 -0600

    CRE2024-41-my-managements-page-&-add-management-page

--- src/app/pages/home/features/managements/pages/add-management/add-management.component.ts
+++ src/app/pages/home/features/managements/pages/add-management/add-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'add-management-component',
@@ -6,7 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-management.component.scss']
 })
 export class AddManagementPageComponent implements OnInit {
-  constructor() { }
+  managementOptions = [
+    {
+      name: 'Aumentar limite de credito',
+      route: '/home/managements/increase-limit'
+    },
+    {
+      name: 'Renovar Tarjeta',
+      route: '/home/managements/renew-cards'
+    },
+    {
+      name: 'Tarjeta robada o extraviada',
+      route: '/home/managements/rob-or-loss'
+    },
+    {
+      name: 'Trasladar Saldo',
+      route: ''
+    },
+    {
+      name: 'Solicitar tarjeta adicional',
+      route: ''
+    },
+    {
+      name: 'Tarjeta danada',
+      route: '/home/managements/damaged-card'
+    },
+    {
+      name: 'Ver o cambiar PIN de tarjeta',
+      route: '/home/managements/card-pin-codes'
+    },
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void { }
+
+  navigateTo(route: string) {
+    if(!route) return
+
+    this.router.navigate([route])
+  }
 }
--- src/app/pages/home/features/managements/pages/my-managements/my-managements.component.ts
+++ src/app/pages/home/features/managements/pages/my-managements/my-managements.component.ts
@@ -6,7 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-managements.component.scss']
 })
 export class MyManagementsPageComponent implements OnInit {
+  STATUSES = {
+    [1]: 'En Proceso',
+    [2]: 'En Espera',
+    [3]: 'Finalizada'
+  }
+
+  MANAGEMENT_TYPES = {
+    [1]: 'Robo o extravio',
+    [2]: 'Renovar Tarjeta',
+    [3]: 'Tarjeta dañada',
+    [4]: 'Pin de tarjetas',
+    [5]: 'Aumentar limite de credito'
+  }
+
+  managements = [
+    {
+      id: 12345678,
+      type: 1,
+      status: 1,
+      requestedDate: '23 Feb 22',
+      endDate: null
+    },
+    {
+      id: 12345679,
+      type: 2,
+      status: 1,
+      requestedDate: '27 Feb 22',
+      endDate: null
+    },
+    {
+      id: 12345680,
+      type: 3,
+      status: 3,
+      requestedDate: '01 Mar 22',
+      endDate: '03 Mar 22'
+    },
+    {
+      id: 12345681,
+      type: 4,
+      status: 2,
+      requestedDate: '02 Mar 22',
+      endDate: null
+    },
+    {
+      id: 12345682,
+      type: 5,
+      status: 3,
+      requestedDate: '01 Mar 22',
+      endDate: '05 Mar 22'
+    }
+  ]
+
+  managementToDisplay: any;
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  displayDetails(index: number) {
+    this.managementToDisplay = this.managements[index]
+  }
 }
