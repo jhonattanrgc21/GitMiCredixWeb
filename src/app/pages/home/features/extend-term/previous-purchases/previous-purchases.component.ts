11d2d5035bde9a978048b035e4daa39b8d925dd3
Rolman Vargas <rvargasp@credix.com>
2024-09-24 09:17:49 -0600

    function duplicada

--- src/app/pages/home/features/extend-term/previous-purchases/previous-purchases.component.ts
+++ src/app/pages/home/features/extend-term/previous-purchases/previous-purchases.component.ts
@@ -301,21 +301,6 @@ export class PreviousPurchasesComponent implements OnInit, OnDestroy {
         (deactivation) => deactivation.PSD_Id === this.operationId
       );
       if (!productInfo.status) {
-        console.log(productInfo);
-        this.message = productInfo.descriptionOne;
-        this.title = productInfo.titleOne;
-        this.done = true;
-        this.template = this.disabledTemplate;
-      }
-    });
-  }
-
-  checkCutDate() {
-    this.extendTermService.checkCutDate().subscribe(({ json }) => {
-      const productInfo = json.deactivationList.find(
-        (deactivation) => deactivation.PSD_Id === this.operationId
-      );
-      if (!productInfo.status) {
         this.message = productInfo.descriptionOne;
         this.title = productInfo.titleOne;
         this.done = true;
