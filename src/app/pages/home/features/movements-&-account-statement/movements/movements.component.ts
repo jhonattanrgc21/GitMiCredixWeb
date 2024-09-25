6dbcc8bcb231e4dd1901a269064f6ade3ad83a56
Daniel Hernandez <dahernandez@intelix.biz>
2024-09-25 14:32:11 -0400

    Checar cutdate antes de redirigir a Ampliar Plazo

--- src/app/pages/home/features/movements-&-account-statement/movements/movements.component.ts
+++ src/app/pages/home/features/movements-&-account-statement/movements/movements.component.ts
@@ -1,10 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {MovementsService} from './movements.service';
 import {Movement} from '../../../../../shared/models/movement';
 import {TagsService} from '../../../../../core/services/tags.service';
 import {Tag} from '../../../../../shared/models/tag';
 import {NavigationService} from '../../../../../core/services/navigation.service';
-
+import {ModalService} from '../../../../../core/services/modal.service';
+import {ExtendTermService} from '../../extend-term/extend-term.service';
+import {Router} from '@angular/router';
+import {
+  CredixResultNotificationComponent
+} from '../../../../../shared/components/credix-result-notification/credix-result-notification.component';
 @Component({
   selector: 'app-movements',
   templateUrl: './movements.component.html',
@@ -19,10 +24,16 @@ export class MovementsComponent implements OnInit, OnDestroy {
   columnTwoTag: string;
   columnThreeTag: string;
   columnFourTag: string;
+  private operationId = 1;
+  message = '';
+  title = '';
+  @ViewChild('modalTemplate') modalTemplate: TemplateRef<any>;
 
-  constructor(private movementsService: MovementsService,
+  constructor(private router: Router,
+              private movementsService: MovementsService,
               private navigationService: NavigationService,
-              private tagsService: TagsService) {
+              private tagsService: TagsService,
+              private modalService: ModalService) {
   }
 
   ngOnInit(): void {
@@ -31,8 +42,22 @@ export class MovementsComponent implements OnInit, OnDestroy {
       this.getTags(functionality.find(fun => fun.description === 'Movimientos').tags));
   }
 
-  onSubmenuChanged() {
-    this.navigationService.submenuChanged('extend-term');
+  onSubmenuChanged(movementId: number) {
+    this.movementsService.checkCutDate().subscribe(({ json }) => {
+      const productInfo = json.deactivationList.find(
+        (deactivation) => deactivation.PSD_Id === this.operationId
+      );
+      if (productInfo.status) {
+        this.router.navigate(['/home/extend-term/recent', movementId]);
+        this.navigationService.submenuChanged('extend-term');
+      } else {
+        this.title = productInfo.titleOne;
+        this.message = productInfo.descriptionOne;
+        this.modalService.open({
+          template: this.modalTemplate,
+        }, { width: 380, disableClose: false, panelClass: 'credix-popup-panel'});
+      }
+    });
   }
 
   getTags(tags: Tag[]) {
